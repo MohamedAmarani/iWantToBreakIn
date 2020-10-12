173e45ad504780289c406e390d5e2baec0c49816
yuanyuzhou <zyuanyu998@gmail.com>
2020-10-12 14:35:33 +0200

    z

--- 2DGame/02-Bubble/02-Bubble/Ball.cpp
+++ 2DGame/02-Bubble/02-Bubble/Ball.cpp
@@ -1,7 +1,9 @@
 #include <cmath>
 #include <iostream>
+#include <stdlib.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
+#include <time.h>
 #include "Ball.h"
 #include "Game.h"
 
@@ -11,46 +13,53 @@
 #define FALL_STEP 4
 
 
-enum PlayerAnims
-{
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT, MOVE_UP, MOVE_DOWN, STAND_UP, STAND_DOWN
-};
-
 
 void Ball::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	xSpeed = ySpeed = -1;
 	spritesheet.loadFromFile("images/ball.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0, 0.25), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(1);
-
-	sprite->setAnimationSpeed(STAND_LEFT, 8);
-	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 1.f));
-
-	sprite->changeAnimation(0);
+	//up-left coner point
+	srand(time(NULL));
 	tileMapDispl = tileMapPos;
-	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posBall.x), float(tileMapDispl.y + posBall.y)));
+	//direction = STOP;
 
 }
 
 void Ball::update(int deltaTime)
 {
 	sprite->update(deltaTime);
+	Move();
+	if (map->collisionMoveLeft(posBall, glm::ivec2(32, 32))) {
+		changeDirection((eDir)((rand() % 3) + 4));
+		Move();
+		//xSpeed = 1;
+	}
 
-	if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
-		xSpeed = 1;
-	if (map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
-		xSpeed = -1;
-	if (map->collisionMoveUp(posPlayer, glm::ivec2(32, 32)))
-		ySpeed = 1;
-	if (map->collisionMoveDown(posPlayer, glm::ivec2(32, 32)))
-		ySpeed = -1;
+	if (map->collisionMoveRight(posBall, glm::ivec2(32, 32))) {
+		//inicialize 4
+		changeDirection((eDir)((rand() % 3) + 1));
+		Move();
+		//xSpeed = -1;
+	}
+	if (map->collisionMoveDown(posBall, glm::ivec2(32, 32))) {
+		changeDirection(direction == DOWNRIGHT ? UPRIGHT : UPLEFT);
+		Move();
+		//ySpeed = 1;
+	}
 
+	//top collision
+	if (map->collisionMoveUp(posBall, glm::ivec2(32, 32))) {
+		changeDirection(direction == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+		Move();
+		//ySpeed = -1;
+	}
 
-	posPlayer.x += xSpeed;
-		posPlayer.y += ySpeed;
+	/*posBall.x += xSpeed;
+		posBall.y += ySpeed;
 
-	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posBall.x), float(tileMapDispl.y + posBall.y)));*/
 }
 
 void Ball::render()
@@ -65,8 +74,56 @@ void Ball::setTileMap(TileMap *tileMap)
 
 void Ball::setPosition(const glm::vec2 &pos)
 {
-	posPlayer = pos;
-	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+	posBall = pos;
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posBall.x), float(tileMapDispl.y + posBall.y)));
 }
 
+void Ball::changeDirection(eDir d) {
+	direction = d;
+}
 
+void Ball::randomDirection()
+{
+	direction = (eDir)((rand() % 6) + 1);
+}
+
+void Ball::Move()
+{
+	switch (direction)
+	{
+	case STOP:
+		break;
+	case LEFT:
+		xSpeed = -1;
+		break;
+	case RIGHT:
+		xSpeed = 1;
+	case UPLEFT:
+		xSpeed = -1;
+		ySpeed = -1;
+		break;
+	case DOWNLEFT:
+		xSpeed = -1;
+		ySpeed = 1;
+		break;
+	case UPRIGHT:
+		xSpeed = 1;
+		ySpeed = -1;
+		break;
+	case DOWNRIGHT:
+		xSpeed = 1;
+		ySpeed = 1;
+		break;
+	default:
+		break;
+	}
+	posBall.x += xSpeed;
+	posBall.y += ySpeed;
+
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posBall.x), float(tileMapDispl.y + posBall.y)));
+}
+
+eDir Ball::getDirection()
+{
+	return direction;
+}
