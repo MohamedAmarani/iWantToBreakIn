1d8b88ec1fee4beb5de5830497057b3cdad83071
yuanyuzhou <zyuanyu998@gmail.com>
2020-10-18 18:06:57 +0200

    imagenes

--- 2DGame/02-Bubble/02-Bubble/Player.cpp
+++ 2DGame/02-Bubble/02-Bubble/Player.cpp
@@ -18,39 +18,39 @@ enum PlayerAnims
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/bub1.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	spritesheet.loadFromFile("images/bat.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(8);
 
 	sprite->setAnimationSpeed(STAND_LEFT, 8);
-	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.5f));
+	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.25f, 0.75f));
 
 	sprite->setAnimationSpeed(STAND_RIGHT, 8);
-	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.75f, 0.75f));
+	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.25f));
 
 	sprite->setAnimationSpeed(STAND_UP, 8);
-	sprite->addKeyframe(STAND_UP, glm::vec2(0.f, 0.25f));
+	sprite->addKeyframe(STAND_UP, glm::vec2(0.25f, 0.50f));
 
 	sprite->setAnimationSpeed(STAND_DOWN, 8);
-	sprite->addKeyframe(STAND_DOWN, glm::vec2(0.f, 0.f));
+	sprite->addKeyframe(STAND_DOWN, glm::vec2(0.25f, 0.f));
 
 	sprite->setAnimationSpeed(MOVE_LEFT, 8);
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.25f, 0.5f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.5f, 0.5f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.75f, 0.5f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.75f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.25f, 0.75f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.5f, 0.75f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.75f, 0.75f));
 
 	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.75f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.75f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.50, 0.75f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.75, 0.75f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.25f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.50, 0.25f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.75, 0.25f));
 
 	sprite->setAnimationSpeed(MOVE_UP, 8);
-	sprite->addKeyframe(MOVE_UP, glm::vec2(0.f, 0.25f));
-	sprite->addKeyframe(MOVE_UP, glm::vec2(0.25, 0.25f));
-	sprite->addKeyframe(MOVE_UP, glm::vec2(0.50, 0.25f));
-	sprite->addKeyframe(MOVE_UP, glm::vec2(0.75, 0.25f));
+	sprite->addKeyframe(MOVE_UP, glm::vec2(0.f, 0.50f));
+	sprite->addKeyframe(MOVE_UP, glm::vec2(0.25, 0.50f));
+	sprite->addKeyframe(MOVE_UP, glm::vec2(0.50, 0.50f));
+	sprite->addKeyframe(MOVE_UP, glm::vec2(0.75, 0.50f));
 
 	sprite->setAnimationSpeed(MOVE_DOWN, 8);
 	sprite->addKeyframe(MOVE_DOWN, glm::vec2(0.f, 0.f));
@@ -58,7 +58,7 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite->addKeyframe(MOVE_DOWN, glm::vec2(0.50, 0.f));
 	sprite->addKeyframe(MOVE_DOWN, glm::vec2(0.75, 0.f));
 
-	sprite->changeAnimation(0);
+	sprite->changeAnimation(STAND_DOWN);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
 
