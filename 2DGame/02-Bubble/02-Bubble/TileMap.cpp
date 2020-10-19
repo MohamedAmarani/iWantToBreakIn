7eaa580185435edd22df99d4c222d6e2981b41e3
yuanyuzhou <zyuanyu998@gmail.com>
2020-10-20 00:43:18 +0200

    style

--- 2DGame/02-Bubble/02-Bubble/TileMap.cpp
+++ 2DGame/02-Bubble/02-Bubble/TileMap.cpp
@@ -65,8 +65,9 @@ bool TileMap::loadLevel(const string &levelFile)
 		return false;
 	getline(fin, line);
 	sstream.str(line);
-	mapSize.x = 13;
-	mapSize.y = 65;
+	//mapSize.x = 13;
+	//mapSize.y = 65;
+	sstream >> mapSize.x >> mapSize.y;
 	getline(fin, line);
 	sstream.str(line);
 	//sstream >> tileSize.x >> tileSize.x >> blockSize;
@@ -181,7 +182,7 @@ bool TileMap::collisionMoveLeft(const glm::ivec2 &pos, const glm::ivec2 &size, i
 	for (int y = y0; y <= y1; y++)
 	{
 		if (map[(y + (offset * 32))*mapSize.x + x] != 0) {
-			if(b == 1 && map[(y + (offset * 32))*mapSize.x + x] != 8)
+			if(b == 1 && (map[(y + (offset * 32))*mapSize.x + x] != 1 && map[(y + (offset * 32))*mapSize.x + x] != 2 && map[(y + (offset * 32))*mapSize.x + x] != 3))
 				map[(y + (offset * 32))*mapSize.x + x] = 0;
 			offset = 0;
 			return true;
@@ -210,7 +211,7 @@ bool TileMap::collisionMoveRight(const glm::ivec2 &pos, const glm::ivec2 &size, 
 	for (int y = y0; y <= y1; y++)
 	{
 		if (map[(y + (offset * 32))*mapSize.x + x] != 0){
-			if (b == 1 && map[(y + (offset * 32))*mapSize.x + x] != 8)
+			if (b == 1 && (map[(y + (offset * 32))*mapSize.x + x] != 1 && map[(y + (offset * 32))*mapSize.x + x] != 2 && map[(y + (offset * 32))*mapSize.x + x] != 3))
 				map[(y + (offset * 32))*mapSize.x + x] = 0;
 			return true;
 		}
@@ -236,7 +237,7 @@ bool TileMap::collisionMoveUp(const glm::ivec2 &pos, const glm::ivec2 &size, int
 	for (int x = x0; x <= x1; x++)
 	{
 		if (map[(y + (offset * 32))*mapSize.x + x] != 0){
-			if (b == 1 && map[(y + (offset * 32))*mapSize.x + x] != 8)
+			if (b == 1 && (map[(y + (offset * 32))*mapSize.x + x] != 1 && map[(y + (offset * 32))*mapSize.x + x] != 2 && map[(y + (offset * 32))*mapSize.x + x] != 3))
 				map[(y + (offset * 32))*mapSize.x + x] = 0;
 			return true;
 		}
@@ -265,7 +266,7 @@ bool TileMap::collisionMoveDown(const glm::ivec2 &pos, const glm::ivec2 &size, i
 	for (int x = x0; x <= x1; x++)
 	{
 		if (map[(y + (offset * 32))*mapSize.x + x] != 0) {
-			if (b == 1 && map[(y + (offset * 32))*mapSize.x + x] != 8)
+			if (b == 1 && (map[(y + (offset * 32))*mapSize.x + x] != 1 && map[(y + (offset * 32))*mapSize.x + x] != 2 && map[(y + (offset * 32))*mapSize.x + x] != 3))
 				map[(y + (offset * 32))*mapSize.x + x] = 0;
 			return true;
 		}
